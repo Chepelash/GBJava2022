034da751b2629b58fc3a089bb8d1ad09d7d4dd9d
Anton Chepelev <chepelash@gmail.com>
2022-09-06 09:36:11 +0300

    Created empty project

---
+++ homework2/src/main/java/ru/chepelash/gb2022/java/hw2/App.java
@@ -0,0 +1,13 @@
+package ru.chepelash.gb2022.java.hw2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
