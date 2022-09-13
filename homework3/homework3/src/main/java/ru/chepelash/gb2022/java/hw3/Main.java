70a773c41ca3079e16f339b3158f58054b35f7d4
Anton Chepelev <chepelash@gmail.com>
2022-09-13 09:59:23 +0300

    Tests are done

---
+++ homework3/homework3/src/main/java/ru/chepelash/gb2022/java/hw3/Main.java
@@ -0,0 +1,23 @@
+package ru.chepelash.gb2022.java.hw3;
+
+import org.javatuples.Triplet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    /*
+     * 1. Пусть дан произвольный список целых чисел, удалить из него четные числа
+     */
+    public static List<Integer> task1(List<Integer> integerList) {
+
+        return null;
+    }
+
+    /*
+     * 2. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+     */
+    public static Triplet<Integer, Integer, Integer> task2(ArrayList<Integer> integerList) {
+        return null;
+    }
+}
---
+++ homework3/homework3/src/test/java/ru/chepelash/gb2022/java/hw3/MainTest.java
@@ -0,0 +1,29 @@
+package ru.chepelash.gb2022.java.hw3;
+
+import org.javatuples.Triplet;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainTest {
+
+    @Test
+    public void task1() {
+        // 1. Пусть дан произвольный список целых чисел, удалить из него четные числа
+        List<Integer> testList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> expectedList = List.of(1, 3, 5, 7, 9);
+        List<Integer> resultList = Main.task1(testList);
+        Assert.assertEquals(expectedList, resultList);
+    }
+
+    @Test
+    public void task2() {
+        // 2. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+        ArrayList<Integer> testArrayList = (ArrayList<Integer>) List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Triplet<Integer, Integer, Integer> expectedTriplet = new Triplet<>(10, 1, 5);
+        Triplet<Integer, Integer, Integer> resultTriplet = Main.task2(testArrayList);
+        Assert.assertEquals(expectedTriplet, resultTriplet);
+    }
+}
