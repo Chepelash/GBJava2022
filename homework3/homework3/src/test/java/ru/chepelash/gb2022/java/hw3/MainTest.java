05835f97d13ccce621dd164169843683ad1157b2
Anton Chepelev <chepelash@gmail.com>
2022-09-13 10:12:35 +0300

    Changed test, because average value is a double

--- homework3/homework3/src/test/java/ru/chepelash/gb2022/java/hw3/MainTest.java
+++ homework3/homework3/src/test/java/ru/chepelash/gb2022/java/hw3/MainTest.java
@@ -21,9 +21,9 @@ public class MainTest {
     @Test
     public void task2() {
         // 2. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
-        ArrayList<Integer> testArrayList = (ArrayList<Integer>) List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        Triplet<Integer, Integer, Integer> expectedTriplet = new Triplet<>(10, 1, 5);
-        Triplet<Integer, Integer, Integer> resultTriplet = Main.task2(testArrayList);
+        ArrayList<Integer> testArrayList = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        Triplet<Integer, Integer, Double> expectedTriplet = new Triplet<>(10, 1, 5.5);
+        Triplet<Integer, Integer, Double> resultTriplet = Main.task2(testArrayList);
         Assert.assertEquals(expectedTriplet, resultTriplet);
     }
 }
