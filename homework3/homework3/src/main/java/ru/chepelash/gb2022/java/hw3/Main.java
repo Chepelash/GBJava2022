214fbf21d7c9a48caa7117a6491b9da7f65b951e
Anton Chepelev <chepelash@gmail.com>
2022-09-13 10:12:47 +0300

    task1 and task2 are done

--- homework3/homework3/src/main/java/ru/chepelash/gb2022/java/hw3/Main.java
+++ homework3/homework3/src/main/java/ru/chepelash/gb2022/java/hw3/Main.java
@@ -2,22 +2,28 @@ package ru.chepelash.gb2022.java.hw3;
 
 import org.javatuples.Triplet;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     /*
      * 1. Пусть дан произвольный список целых чисел, удалить из него четные числа
      */
     public static List<Integer> task1(List<Integer> integerList) {
-
-        return null;
+        List<Integer> resultList = new ArrayList<>(List.copyOf(integerList));
+        resultList.removeIf(integer -> integer % 2 == 0);
+        return resultList;
     }
 
     /*
      * 2. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
      */
-    public static Triplet<Integer, Integer, Integer> task2(ArrayList<Integer> integerList) {
-        return null;
+    public static Triplet<Integer, Integer, Double> task2(ArrayList<Integer> integerList) {
+        int maxValue = integerList.stream().mapToInt(v -> v)
+                .max().orElseThrow(NoSuchElementException::new);
+        int minValue = integerList.stream().mapToInt(v -> v)
+                .min().orElseThrow(NoSuchElementException::new);
+        double avrValue = integerList.stream().mapToInt(v -> v)
+                .average().orElseThrow(NoSuchElementException::new);
+        return new Triplet<>(maxValue, minValue, avrValue);
     }
 }
