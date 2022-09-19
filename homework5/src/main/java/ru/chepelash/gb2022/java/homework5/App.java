39b0a1767e8a528d8ea0e46ba3db208040028faf
Anton Chepelev <chepelash@gmail.com>
2022-09-19 21:02:41 +0300

    created project

---
+++ homework5/src/main/java/ru/chepelash/gb2022/java/homework5/App.java
@@ -0,0 +1,13 @@
+package ru.chepelash.gb2022.java.homework5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
