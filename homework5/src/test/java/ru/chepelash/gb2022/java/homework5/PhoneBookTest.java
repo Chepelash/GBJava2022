fd9ff65105ae024b7de38b647943ec2e420db570
Anton Chepelev <chepelash@gmail.com>
2022-09-19 21:19:21 +0300

    Starting tests

---
+++ homework5/src/test/java/ru/chepelash/gb2022/java/homework5/PhoneBookTest.java
@@ -0,0 +1,24 @@
+package ru.chepelash.gb2022.java.homework5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhoneBookTest {
+
+    @Test
+    void getNumbersByName() {
+    }
+
+    @Test
+    void getNumberByNameAndTag() {
+    }
+
+    @Test
+    void addNumberForPerson() {
+    }
+
+    @Test
+    void editNumber() {
+    }
+}
