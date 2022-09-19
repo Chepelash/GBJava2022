0c03eb9e7c1c14761f9774d455822cf9ffbfd060
Anton Chepelev <chepelash@gmail.com>
2022-09-19 22:01:02 +0300

    Created methods

--- homework5/src/main/java/ru/chepelash/gb2022/java/homework5/PhoneBook.java
+++ homework5/src/main/java/ru/chepelash/gb2022/java/homework5/PhoneBook.java
@@ -16,14 +16,23 @@ public class PhoneBook {
     /*
      * returns list of phone numbers for a person
      */
-    public List<String> getNumbersByName(String name){
-        return null;
+    public HashMap<String, String> getNumbersByName(String name)
+            throws IllegalArgumentException {
+        if(!phoneBook.containsKey(name)){
+            throw new IllegalArgumentException("Person does not exists");
+        }
+        return phoneBook.get(name);
     }
     /*
      * returns specific number for a person
      */
-    public String getNumberByNameAndTag(String name, String tag){
-        return null;
+    public String getNumberByNameAndTag(String name, String tag)
+            throws IllegalArgumentException{
+        HashMap<String, String> phones = getNumbersByName(name);
+        if(!phones.containsKey(tag)){
+            throw new IllegalArgumentException("Tag does not exists");
+        }
+        return phones.get(tag);
     }
 
     /*
@@ -32,14 +41,32 @@ public class PhoneBook {
      * if person exists and tag does not - adds new phone number with this tag
      * if person and tag exist - throws an exception
      */
-    public void addNumberForPerson(String name, String tag, String phoneNumber) {
-
+    public void addNumberForPerson(String name, String tag, String phoneNumber)
+            throws IllegalArgumentException {
+        if(phoneBook.containsKey(name)){
+            HashMap<String, String> phones = getNumbersByName(name);
+            if(phones.containsKey(tag)){
+                throw new IllegalArgumentException("Person and tag exist");
+            }
+            phones.put(tag, phoneNumber);
+        } else {
+            HashMap<String, String> phone = new HashMap<>();
+            phone.put(tag, phoneNumber);
+            phoneBook.put(name, phone);
+        }
     }
     /*
      * edits existing phone number
      * if person or tag does not exist - throws an exception
      */
     public void editNumber(String name, String tag, String phoneNumber){
-
+        if(!phoneBook.containsKey(name)){
+            throw new IllegalArgumentException("Person does not exist");
+        }
+        HashMap<String, String> phones = getNumbersByName(name);
+        if(!phones.containsKey(tag)){
+            throw new IllegalArgumentException("Tag does not exist");
+        }
+        phones.put(tag, phoneNumber);
     }
 }
