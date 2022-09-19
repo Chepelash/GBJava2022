fd5cbd03082aa5bad6ecf2b96f88a7175ce44bdc
Anton Chepelev <chepelash@gmail.com>
2022-09-19 22:49:03 +0300

    created class

---
+++ homework5/src/main/java/ru/chepelash/gb2022/java/homework5/NameSorter.java
@@ -0,0 +1,15 @@
+package ru.chepelash.gb2022.java.homework5;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+ * Пусть дан список сотрудников
+ * Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
+ * Отсортировать по убыванию популярности.
+ */
+public class NameSorter {
+    public static void countAndSortNames(List<String> nameList){
+
+    }
+}
