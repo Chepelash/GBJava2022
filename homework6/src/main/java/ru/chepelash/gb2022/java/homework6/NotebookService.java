bc1f8389f9269c20022d6cc96abb0861cec2a145
Anton Chepelev <chepelash@gmail.com>
2022-09-24 10:46:30 +0300

    refactor

--- homework6/src/main/java/ru/chepelash/gb2022/java/homework6/NotebookService.java
+++ homework6/src/main/java/ru/chepelash/gb2022/java/homework6/NotebookService.java
@@ -4,13 +4,13 @@ import ru.chepelash.gb2022.java.homework6.enums.ColorEnum;
 import ru.chepelash.gb2022.java.homework6.enums.OsEnum;
 import ru.chepelash.gb2022.java.homework6.enums.VendorEnum;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class NotebookService {
     private NotebookCollection notebookCollection;
+    private final Logger LOGGER = Logger.getLogger(NotebookService.class.getName());
 
     public NotebookService(NotebookCollection notebookCollection) {
         this.notebookCollection = notebookCollection;
@@ -40,75 +40,62 @@ public class NotebookService {
     public HashMap<String, NotebookDTO> getAllNotebooks(){
         return notebookCollection.getNotebooks();
     }
-    public HashMap<String, NotebookDTO> getNotebooksByColors(List<ColorEnum> colors){
-        if(notebookCollection.isEmpty()){
-            return null;
-        }
-        List<Map.Entry<String, NotebookDTO>> filteredEntries = notebookCollection.getNotebooks().entrySet().stream()
-                .filter(e -> colors.contains(e.getValue().getColor()))
-                .collect(Collectors.toList());
-        if(filteredEntries.isEmpty()){
-            return null;
-        }
+    private HashMap<String, NotebookDTO> getNotebookByParameter(int parameterValue, String parameterName) {
         HashMap<String, NotebookDTO> result = new HashMap<>();
+        if(parameterValue < 0){
+            return result;
+        }
+        List<Map.Entry<String, NotebookDTO>> filteredEntries;
+        switch (parameterName){
+            case "hdd":
+                filteredEntries = notebookCollection.getNotebooks().entrySet().stream()
+                        .filter(e -> e.getValue().getHddSize() >= parameterValue)
+                        .collect(Collectors.toList());
+                break;
+            case "ozu":
+                filteredEntries = notebookCollection.getNotebooks().entrySet().stream()
+                        .filter(e -> e.getValue().getOzu() >= parameterValue)
+                        .collect(Collectors.toList());
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong parameter");
+        }
+        for (Map.Entry<String, NotebookDTO> entry:
+                filteredEntries) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+    private HashMap<String, NotebookDTO> collectHashMap(List<Map.Entry<String, NotebookDTO>> filteredEntries){
+        HashMap<String, NotebookDTO> result = new HashMap<>();
+        if(filteredEntries.isEmpty()){
+            return result;
+        }
         for (Map.Entry<String, NotebookDTO> entry:
              filteredEntries) {
             result.put(entry.getKey(), entry.getValue());
         }
         return result;
     }
+    public HashMap<String, NotebookDTO> getNotebooksByColors(List<ColorEnum> colors){
+        List<Map.Entry<String, NotebookDTO>> filteredEntries = notebookCollection.getNotebooks().entrySet().stream()
+                .filter(e -> colors.contains(e.getValue().getColor()))
+                .collect(Collectors.toList());
+        return collectHashMap(filteredEntries);
+    }
 
     public HashMap<String, NotebookDTO> getNotebooksByOs(List<OsEnum> osEnums){
-        if(notebookCollection.isEmpty()){
-            return null;
-        }
         List<Map.Entry<String, NotebookDTO>> filteredEntries = notebookCollection.getNotebooks().entrySet().stream()
                 .filter(e -> osEnums.contains(e.getValue().getOs()))
                 .collect(Collectors.toList());
-        if(filteredEntries.isEmpty()){
-            return null;
-        }
-        HashMap<String, NotebookDTO> result = new HashMap<>();
-        for (Map.Entry<String, NotebookDTO> entry:
-                filteredEntries) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-        return result;
+        return collectHashMap(filteredEntries);
     }
 
     public HashMap<String, NotebookDTO> getNotebooksByOzu(int ozu){
-        if(ozu < 0){
-            return null;
-        }
-        List<Map.Entry<String, NotebookDTO>> filteredEntries = notebookCollection.getNotebooks().entrySet().stream()
-                .filter(e -> e.getValue().getOzu() >= ozu)
-                .collect(Collectors.toList());
-        if(filteredEntries.isEmpty()){
-            return null;
-        }
-        HashMap<String, NotebookDTO> result = new HashMap<>();
-        for (Map.Entry<String, NotebookDTO> entry:
-                filteredEntries) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-        return result;
+        return getNotebookByParameter(ozu, "ozu");
     }
 
     public HashMap<String, NotebookDTO> getNotebooksByHdd(int hdd){
-        if(hdd < 0){
-            return null;
-        }
-        List<Map.Entry<String, NotebookDTO>> filteredEntries = notebookCollection.getNotebooks().entrySet().stream()
-                .filter(e -> e.getValue().getHddSize() >= hdd)
-                .collect(Collectors.toList());
-        if(filteredEntries.isEmpty()){
-            return null;
-        }
-        HashMap<String, NotebookDTO> result = new HashMap<>();
-        for (Map.Entry<String, NotebookDTO> entry:
-                filteredEntries) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-        return result;
+        return getNotebookByParameter(hdd, "hdd");
     }
 }
